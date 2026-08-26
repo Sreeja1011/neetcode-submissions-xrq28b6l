2b81916e97ecc2825d9bef2673823236334ea8a8
Koditala Sreeja <96378239+Sreeja1011@users.noreply.github.com>
2026-08-26 22:37:07 +0530

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    HashMap<Integer, Integer> memo = new HashMap<>();
+    public int dfs(int[] coins, int amount){
+        if(amount==0){
+            return 0;
+        }
+        if(memo.containsKey(amount)){
+            return memo.get(amount);
+        }
+        int res=Integer.MAX_VALUE;
+        for(int n:coins){
+            if(amount-n>=0){
+                int result=dfs(coins,amount-n);
+                if(result!=Integer.MAX_VALUE){
+                    res=Math.min(res,1+result);
+                }
+            }
+        }
+        memo.put(amount,res);
+        return res;
+
+    }
+    public int coinChange(int[] coins, int amount) {
+        if(amount==0){
+            return 0;
+        }
+        int minCoins = dfs(coins,amount);
+        return minCoins == Integer.MAX_VALUE ? -1 : minCoins;
+
+    }
+}
