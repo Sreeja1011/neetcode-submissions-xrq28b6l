e6788e3c719b895cc2bb9487e67ace8dfa517d6a
Koditala Sreeja <96378239+Sreeja1011@users.noreply.github.com>
2026-08-28 22:50:25 +0530

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n=coins.length;
+        if(amount==0){
+            return 0;
+        }
+        int dp[][]=new int[n+1][amount+1];
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<amount+1;i++){
+            dp[0][i]=amount+1;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<amount+1;j++){
+                if(j<coins[i-1]){
+                    dp[i][j]=dp[i-1][j];
+                }
+                else{
+                dp[i][j]=Math.min(dp[i-1][j],dp[i][j-coins[i-1]]+1);
+                }
+
+            }
+
+        }
+        return dp[n][amount] > amount ? -1 : dp[n][amount];
+    }
+}
